08a325903b31841c6330076fb5b5e936da2fdcd5
yusufqx <117523468+yusufqx@users.noreply.github.com>
2022-11-06 00:08:42 +0300

    Update adamasmaca.py
    bu oyun basit bir adam asmaca oyunudur 
    
    random kütüphanesi olmadığı için kodun başındaki cevap="kedi" kodundaki kedi yerine başka bir hayvan girin.(liste içindeki hayvanlardan olması gerek)

--- adamasmaca.py
+++ adamasmaca.py
@@ -7,13 +7,19 @@ import time
 kelimeler=["kuş","kedi","köpek","kaplumbağa","balık","kartal","kurbağa","ayı","arı","domuz","sinek","yarasa"]
 for harf in kelimeler:
     cevap="kedi"
-
+hak=5
 while True:
     tahmin=input("tahmin yazınız..")
     if tahmin not in kelimeler:
         print("lütfen geçerli bir kelime giriniz..")
-
+        hak==5
+        print("  --------  ")
+        time.sleep(0.5)
+        print("     O      ")
+        time.sleep(0.5)
+        print("     |      ")
     elif tahmin!="kedi":
+        time.sleep(0.5)
         print("  --------  ")
         time.sleep(0.5)
         print("     O      ")
@@ -23,12 +29,21 @@ while True:
 
     elif tahmin == cevap:
         print("tebrikler doğru tahmin..")
+
         print("tahmininiz : {}\n cevap : {}".format(cevap, cevap))
         break
     tahmin = input("tahmin yazınız..")
     if tahmin not in kelimeler:
         print("lütfen geçerli bir kelime giriniz..")
+        hak == 5
+        print("  --------  ")
+        time.sleep(0.5)
+        print("     O      ")
+        time.sleep(0.5)
+        print("     |      ")
+
     elif tahmin!="kedi":
+        time.sleep(0.5)
         print("  --------  ")
         print("     O      ")
         time.sleep(0.5)
@@ -36,7 +51,9 @@ while True:
         time.sleep(0.5)
         print("    / \     ")
         print("yanlış tahmin")
+        hak==4
     elif tahmin == cevap:
+        time.sleep(0.5)
         print("  --------  ")
         time.sleep(0.5)
         print("     O      ")
@@ -49,7 +66,15 @@ while True:
     tahmin = input("tahmin yazınız..")
     if tahmin not in kelimeler:
         print("lütfen geçerli bir kelime giriniz..")
+        hak==5
+        print("  --------  ")
+        time.sleep(0.5)
+        print("     O      ")
+        time.sleep(0.5)
+        print("     |      ")
+
     elif tahmin!="kedi":
+        time.sleep(0.5)
         print("  --------  ")
         print("   \ O /    ")
         time.sleep(0.5)
@@ -57,8 +82,9 @@ while True:
         time.sleep(0.5)
         print("    / \     ")
         print("yanlış tahmin")
-
+        hak==3
     elif tahmin==cevap:
+        time.sleep(0.5)
         print("  --------  ")
         print("     O      ")
         time.sleep(0.5)
@@ -72,7 +98,14 @@ while True:
     tahmin = input("tahmin yazınız..")
     if tahmin not in kelimeler:
         print("lütfen geçerli bir kelime giriniz..")
+        hak==5
+        print("  --------  ")
+        time.sleep(0.5)
+        print("     O      ")
+        time.sleep(0.5)
+        print("     |      ")
     elif tahmin!="kedi":
+        time.sleep(0.5)
         print("  --------  ")
         print("   \ O /|   ")
         time.sleep(0.5)
@@ -80,13 +113,16 @@ while True:
         time.sleep(0.5)
         print("    / \     ")
         print("yanlış tahmin")
+        hak==2
     elif tahmin==cevap:
+        time.sleep(0.5)
         print("  --------  ")
         print("   \ O /    ")
         time.sleep(0.5)
         print("     |      ")
         time.sleep(0.5)
         print("    / \     ")
+        time.sleep(0.5)
 
         print("tebrikler doğru tahmin..")
         print("tahmininiz : {}\n cevap : {}".format(cevap, cevap))
@@ -94,14 +130,24 @@ while True:
     tahmin = input("tahmin yazınız..")
     if tahmin not in kelimeler:
         print("lütfen geçerli bir kelime giriniz..")
+        hak==5
+        print("  --------  ")
+        time.sleep(0.5)
+        print("     O      ")
+        time.sleep(0.5)
+        print("     |      ")
+
     elif tahmin!="kedi":
+        time.sleep(0.5)
         print("  --------  ")
         print("   \ O_|/   ")
         time.sleep(0.5)
         print("     |      ")
         time.sleep(0.5)
         print("    / \     ")
+        time.sleep(0.5)
         print("yanlış tahmin DİKKAT SON HAKKINIZ")
+        hak==1
     elif tahmin==cevap:
         print("  --------  ")
         print("   \ O /|   ")
@@ -109,31 +155,42 @@ while True:
         print("     |      ")
         time.sleep(0.5)
         print("    / \     ")
-
+        time.sleep(0.5)
         print("tebrikler doğru tahmin..")
         print("tahmininiz : {}\n cevap : {}".format(cevap, cevap))
         break
     tahmin = input("tahmin yazınız..")
     if tahmin not in kelimeler:
         print("lütfen geçerli bir kelime giriniz..")
+        hak==5
+        print("  --------  ")
+        time.sleep(0.5)
+        print("     O      ")
+        time.sleep(0.5)
+        print("     |      ")
     elif tahmin!="kedi":
         print("yanlış tahmin oyun bitti öldünüz")
+
         print("     O_|    ")
+        time.sleep(0.5)
         print("    /|\      ")
+        time.sleep(0.5)
         print("    / \     ")
+        time.sleep(0.2)
         break
-    tahmin = input("tahmin yazınız..")
     if tahmin not in kelimeler:
         print("lütfen geçerli bir kelime giriniz..")
+        hak==5
+        print("  --------  ")
+        time.sleep(0.5)
+        print("     O      ")
+        time.sleep(0.5)
+        print("     |      ")
     elif tahmin==cevap:
+
         print("tebrikler doğru tahmin..")
         print("tahmininiz : {}\n cevap : {}".format(cevap,cevap))
         break
-
-
-
-
-
-
-
+    if hak==0:
+        break
 
